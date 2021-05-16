7071e5501aa4010afc2bd8df17c46debad03776a
rashonmitchell <rashonmitchell313@gmail.com>
2021-05-16 15:24:06 -0500

    removed sql from github

--- queenie.db.elephantsql.com.session.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO members (id, name, age, points, householdId)
-VALUES (
-    id:integer,
-    'name:character varying',
-    'age:character varying',
-    points:integer,
-    householdId:integer
-  );
